e928864e435ae88c9d85ba0a7e1b69928bd65098
Eldin <eldin.kurpejovic@udg.edu.me>
2020-06-28 14:47:46 +0200

    testedipt

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
         </div>        <div id="menu">
             <ul>
                 <li class="{{Request::path()=== '/' ? 'current_page_item':''}}"><a href="https://www.linkedin.com/in/eldin-kurpejovi%C4%87-3444b9109/" accesskey="1" title="">About Me</a></li>
-                <li class="{{Request::path()=== 'careers' ? 'current_page_item':''}}"><a href="/subscribers" accesskey="4" title="">Subscribers</a></li>
+                <li class="{{Request::path()=== 'careers' ? 'current_page_item':''}}"><a href="/subscribers/create" accesskey="4" title="">Subscribe</a></li>
                 <li class="{{Request::path()=== 'contact' ? 'current_page_item':''}}"><a href="https://github.com/E1337Kq/" accesskey="5" title="">Github</a></li>
             </ul>
         </div>
